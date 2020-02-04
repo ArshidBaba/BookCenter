19739944c035c18670ac2367916d8912af24783e
Arshid <arshidbaba93@gmail.com>
2020-02-04 15:12:52 +0530

    changed urls

--- catalog/apiviews.py
+++ catalog/apiviews.py
@@ -6,23 +6,23 @@ from django.shortcuts import get_object_or_404
 from .models import Book, Author, Genre, BookInstance
 from .serializers import BookSerializer, AuthorSerializer, GenreSerializer, BookInstanceSerializer
 
-# class BookDetail(APIView):
-#     def get(self, request, pk):
-#         book = get_object_or_404(Book, pk=pk)
-#         data = BookSerializer(book).data
-#         return Response(data)
+class BookDetail(APIView):
+    def get(self, request, pk):
+        book = get_object_or_404(Book, pk=pk)
+        data = BookSerializer(book).data
+        return Response(data)
 
-# class BookList(APIView):
-#     def get(self, request):
-#         books = Book.objects.all()
-#         data = BookSerializer(books, many=True).data
-#         return Response(data)
+class BookList(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        data = BookSerializer(books, many=True).data
+        return Response(data)
 
-# class AuthorList(APIView):
-#     def get(self, request):
-#         authors = Author.objects.all()
-#         data = AuthorSerializer(authors, many=True).data
-#         return Response(data)
+class AuthorList(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        data = AuthorSerializer(authors, many=True).data
+        return Response(data)
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .apiviews import  BookViewSet, GenreViewSet, BookInstanceViewSet, AuthorViewSet
+from .apiviews import GenreViewSet, AuthorViewSet, BookList, BookDetail, AuthorList, BookViewSet
 # from rest_framework_swagger.views import get_swagger_view
 
 
@@ -10,14 +10,14 @@ from .apiviews import  BookViewSet, GenreViewSet, BookInstanceViewSet, AuthorVie
 router = DefaultRouter()
 router.register('books', BookViewSet)
 router.register('genres', GenreViewSet)
-router.register('<int:pk>/', BookInstanceViewSet)
+# router.register('<int:pk>/', BookInstanceViewSet)
 router.register('authors', AuthorViewSet)
 
 
 # schema_view = get_swagger_view(title='Book Center')
 
 urlpatterns = [
-    # path('', BookList.as_view(), name='index'),
+    # path('books/', BookList.as_view(), name='books'),
     # path('authors/', AuthorList.as_view(), name='authors'),
     # path('catalog/<int:pk>/', BookInstanceViewSet.as_view(), name="catalog" )
     # path('swagger-docs/', schema_view),
