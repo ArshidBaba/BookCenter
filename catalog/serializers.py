7049d1d3acb3084cfc266b12276f9c4aa654b0ce
Arshid <arshidbaba93@gmail.com>
2020-02-25 17:06:57 +0530

    added User serializer

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 from .models import Book, Author, Genre, BookInstance
 # , Query
@@ -28,4 +29,36 @@ class GenreSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Query
 #         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
