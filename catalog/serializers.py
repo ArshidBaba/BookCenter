1d47fff6623af2632014100bfdc933c311f5ec5c
Arshid <arshidbaba93@gmail.com>
2020-03-04 12:38:12 +0530

    edited serializers and urls

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -38,15 +38,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        user = User(
-            email=validated_data['email'],
-            username=validated_data['username']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        Token.objects.create(user=user)
-        return user
 
 
 
